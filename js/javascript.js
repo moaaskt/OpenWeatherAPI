fb7b96b6ad8a2779788d8448e3ee90c11def5a50
moaaskt <moacirmayer@hotmail.com>
2023-07-13 04:09:02 -0300

    Ajustando flags

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,5 @@
 const apiKey = "e615052850b5398fba2a19d0f7e1394b";
-const apiCountry = "https://flagsapi.com/br/BE/shiny/";
+const apiCountry = "https://www.countryflagicons.com/SHINY/64/";
 
 const cityInput = document.querySelector("#city-input");
 const btnSearch = document.querySelector("#search");
@@ -9,7 +9,7 @@ const tempElement = document.querySelector("#temperature span");
 const descElement = document.querySelector("#description");
 const iconWatherElement = document.querySelector("#weather-icon");
 const countryElement = document.querySelector("#country");
-const umidityElement = document.querySelector("#humidity span");
+const humidityElement = document.querySelector("#humidity span");
 const windElement = document.querySelector("#wind span");
 
 //funçoes
@@ -18,16 +18,22 @@ const getWeatherData = async (city) => {
     const res = await fetch(apiWeatherUrl)
     const data = await res.json();
 
-    console.log(data);
+   return data
 
 }
 
 
-const showWeatherData = (city) => {
-    getWeatherData(city)
+const showWeatherData = async (city) => {
+ const data =  await getWeatherData(city)
+
+ cityElement.innerText = data.name;
+ tempElement.innerText  = parseInt(data.main.temp);
+ descElement.innerText  = data.weather[0].description;
+ iconWatherElement.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
+ countryElement.setAttribute("src", apiCountry + data.sys.country)
+ humidityElement.innerText = `${data.main.humidity}%`
+ windElement.innerText = `${data.wind.speed}km/h`
 };
-
-
 // Events
 
 btnSearch.addEventListener("click", (e) => {
