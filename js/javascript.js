283728e85061c9e49b4fdbb015bc6c8aa5b0d4c9
moaaskt <moacirmayer@hotmail.com>
2023-07-14 02:37:39 -0300

    finalizando projeto

--- js/javascript.js
+++ js/javascript.js
@@ -7,41 +7,99 @@ const btnSearch = document.querySelector("#search");
 const cityElement = document.querySelector("#city");
 const tempElement = document.querySelector("#temperature span");
 const descElement = document.querySelector("#description");
-const iconWatherElement = document.querySelector("#weather-icon");
+const iconWeatherElement = document.querySelector("#weather-icon");
 const countryElement = document.querySelector("#country");
 const humidityElement = document.querySelector("#humidity span");
 const windElement = document.querySelector("#wind span");
 
-//funçoes
+const weatherContainer = document.querySelector("#weather-data");
+
+// Funções
 const getWeatherData = async (city) => {
-    const apiWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
-    const res = await fetch(apiWeatherUrl)
-    const data = await res.json();
+  const apiWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  const res = await fetch(apiWeatherUrl);
+  const data = await res.json();
 
-   return data
-
-}
-
+  return data;
+};
 
 const showWeatherData = async (city) => {
- const data =  await getWeatherData(city)
+  try {
+    const data = await getWeatherData(city);
 
- cityElement.innerText = data.name;
- tempElement.innerText  = parseInt(data.main.temp);
- descElement.innerText  = data.weather[0].description;
- iconWatherElement.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
- countryElement.setAttribute("src", apiCountry + data.sys.country)
- humidityElement.innerText = `${data.main.humidity}%`
- windElement.innerText = `${data.wind.speed}km/h`
+    if (data.cod === "404") {
+      throw new Error("Cidade não encontrada");
+    }
+
+    // Remover mensagem de erro, se existir
+    const errorContainer = document.querySelector(".error-container");
+    if (errorContainer) {
+      errorContainer.remove();
+    }
+
+    // Remover dados da cidade anterior
+    clearWeatherData();
+
+    // Exibir os novos dados
+    cityElement.innerText = data.name;
+    tempElement.innerText = parseInt(data.main.temp - 273.15);
+    descElement.innerText = data.weather[0].description;
+    iconWeatherElement.setAttribute(
+      "src",
+      `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+    );
+    countryElement.setAttribute("src", apiCountry + data.sys.country + ".png");
+    humidityElement.innerText = `${data.main.humidity}%`;
+    windElement.innerText = `${data.wind.speed}km/h`;
+
+    weatherContainer.classList.remove("hide");
+  } catch (error) {
+    console.error(error);
+    showError("Não foi possível encontrar o clima da cidade com esse nome.");
+
+    // Remover dados da cidade anterior
+    clearWeatherData();
+  }
 };
-// Events
 
+const showError = (message) => {
+  let errorContainer = document.querySelector(".error-container");
+
+  if (!errorContainer) {
+    errorContainer = document.createElement("div");
+    errorContainer.classList.add("error-container");
+    weatherContainer.appendChild(errorContainer);
+  }
+
+  errorContainer.innerHTML = ""; // Limpar conteúdo existente
+
+  const errorElement = document.createElement("p");
+  errorElement.innerText = message;
+
+  errorContainer.appendChild(errorElement);
+};
+
+const clearWeatherData = () => {
+  cityElement.innerText = "";
+  tempElement.innerText = "";
+  descElement.innerText = "";
+  iconWeatherElement.setAttribute("src", "");
+  countryElement.setAttribute("src", "");
+  humidityElement.innerText = "";
+  windElement.innerText = "";
+};
+
+// Eventos
 btnSearch.addEventListener("click", (e) => {
+  e.preventDefault();
+  const city = cityInput.value;
 
-    e.preventDefault()
-    const city = cityInput.value;
+  showWeatherData(city);
+});
 
-
+cityInput.addEventListener("keyup", (e) => {
+  if (e.key === "Enter") {
+    const city = e.target.value;
     showWeatherData(city);
-
-})
+  }
+});
